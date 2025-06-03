44d60a29ac3260fc7714c4b2c0a15fd7be2d2dab
azir12345 <104439102+azir12345@users.noreply.github.com>
2025-06-04 04:19:46 +0800

    Update alipaynfc_rewrite.js

--- alipaynfc_rewrite.js
+++ alipaynfc_rewrite.js
@@ -1,1 +1,13 @@
+// alipaynfc_rewrite.js
+let url = $request.url;
+let decoded = decodeURIComponent(url);
 
+// 提取 scheme 参数中的 alipay 开头的部分
+let match = decoded.match(/scheme=(alipay:\/\/[^\s]+)/);
+
+if (match && match[1]) {
+  let redirectURL = match[1];
+  $done({ status: 302, headers: { Location: redirectURL } });
+} else {
+  $done({});
+}
